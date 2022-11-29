ea207bdf80421a4fae652a5fa03659b29a7358b3
Jeremiah Ekundayo <dayoolapeju@outlook.com>
2022-11-29 03:13:22 +0100

    Setup login & register views

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/login');
 });
+
+Route::get('/login', fn() => view('login'))->name('login.get');
+
+Route::post('/login', fn() => "POST login")->name('login.post');;
+
+Route::get('/signup', fn() => view('signup'))->name('signup.get');
+
+Route::post('/signup', fn() => "POST signup")->name('signup.post');
+
+
+// TODO Routes
